9cccd27313abda365804954345b80f2e9d6105f9
Marco Parrone <marco@marcoparrone.com>
2021-01-31 15:25:27 +0100

    Fixed slow shadow effect.

--- src/dialog.js
+++ src/dialog.js
@@ -23,14 +23,14 @@ class Dialog extends React.Component {
       'className': 'mdc-dialog', 'role': 'alertdialog', 'aria-modal': 'true', 'aria-labelledby': this.props.id + '-dialog-title',
       'aria-describedby': this.props.id + '-dialog-content', 'id': this.props.id + '-dialog'
     },
-      React.createElement('div', { 'className': 'mdc-dialog__container' },
-        [React.createElement('div', { 'className': 'mdc-dialog__surface', 'key': this.props.id + '-dialog-surface' },
+      [React.createElement('div', { 'className': 'mdc-dialog__container' },
+        React.createElement('div', { 'className': 'mdc-dialog__surface', 'key': this.props.id + '-dialog-surface' },
           [React.createElement('h2', { 'className': 'mdc-dialog__title', 'id': this.props.id + '-dialog-title', 'key': this.props.id + '-dialog-title' }, this.props.title),
           React.createElement('div', { 'className': 'mdc-dialog__content', 'id': this.props.id + '-dialog-content', 'key': this.props.id + '-dialog-content' }, this.props.children),
           React.createElement('footer', { 'className': 'mdc-dialog__actions', 'key': this.props.id + '-dialog-footer' },
             this.props.actions ? this.props.actions : React.createElement('button', { 'type': 'button', 'className': 'mdc-button mdc-dialog__button', 'data-mdc-dialog-action': 'yes' },
-              React.createElement('span', { 'className': 'mdc-button__label' }, this.props.text_close_button ? this.props.text_close_button : "Close")))]),
-        React.createElement('div', { 'className': 'mdc-dialog__scrim', 'key': this.props.id + '-dialog-scrim' })]));
+              React.createElement('span', { 'className': 'mdc-button__label' }, this.props.text_close_button ? this.props.text_close_button : "Close")))])),
+      React.createElement('div', { 'className': 'mdc-dialog__scrim', 'key': this.props.id + '-dialog-scrim' })]);
   }
 }
 
